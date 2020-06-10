220f4eb93885c3d6918a56841f6f4164dcec2ac8
Grace Kim <goooyoon@gmail.com>
2020-06-10 20:30:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,6 +19,15 @@
     end
   end
 
-  
+  def validate_triangle
+    true_triangle = [(a+b>c), (a+c>b), (b+c>a)]
+    [a,b,c].each do |s|
+      true_triangle << false if s <= 0
+    end
+      raise TriangleError if true_triangle.include?(false)
+  end
+
+  class TriangleError < StandardError
+  end
   
 end
